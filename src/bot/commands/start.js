4b79ad46afdb64c22cb07bff24c5db0e13de44ef
appleJax <appleJax@users.noreply.github.com>
2018-10-24 00:21:02 -0500

    add max questions to start usage

--- src/bot/commands/start.js
+++ src/bot/commands/start.js
@@ -9,7 +9,7 @@ export default {
   name: 'start',
   aliases: ['s'],
   description: 'Start a new quiz',
-  usage: '[number of questions (default: 10)]',
+  usage: '[number of questions (default: 10, max: 30)]',
   async execute(msg, args) {
     const self = this;
     let [quizSize] = args;
