7c81f87b1e8449073605f6128def3342e048a74d
appleJax <appleJax@users.noreply.github.com>
2018-11-08 20:22:40 -0600

    refactor upload/spinners/flash-message to use ajax

--- src/admin/public/admin.js
+++ src/admin/public/admin.js
@@ -1,38 +1,119 @@
+let flash;
+let fileInput;
+
 window.onload = () => {
-  setTimeout(hideFlash, 7000);
-  addSpinner('new-deck-form');
+  flash = initFlash();
+  fileInput = initFileInput();
+  wireUp('new-deck-form');
 };
 
-function hideFlash() {
-  const flash = document.querySelector('.banner-container');
-  flash.classList.add('hidden');
-}
-
-function addSpinner(formId) {
-  getSubmitBtns(formId).forEach(submitBtn => submitBtn.addEventListener(
-    'click',
-    showSpinner,
-  ));
-}
-
-function showSpinner(event) {
-  const formId = event.target.id.replace('submit', 'form');
-  const form = document.getElementById(formId);
-
-  const shroud = makeDiv('shroud');
-  const spinner = makeDiv('spinner');
-
-  shroud.appendChild(spinner);
-  form.appendChild(shroud);
-}
-
-function getSubmitBtns(formId) {
+function getSubmitBtn(formId) {
   const submitBtnClass = formId.replace('form', 'submit');
-  return document.querySelectorAll(`.${submitBtnClass}`);
+  return document.querySelector(`.${submitBtnClass}`);
+}
+
+function initFlash() {
+  const flash = document.querySelector('.banner');
+  return {
+    show(type, msg) {
+      flash.classList.add(`banner--${type}`);
+      flash.innerHTML = msg;
+      flash.classList.remove('hidden');
+      setTimeout(this.hide, 1000 * 20);
+    },
+
+    hide() {
+      flash.classList.add('hidden');
+      flash.classList.remove('banner--error');
+      flash.classList.remove('banner--success');
+      flash.classList.remove('banner--warning');
+    }
+  }
+}
+
+function initFileInput() {
+  const fileInput = document.getElementById('file-input');
+  const newDeckForm = document.getElementById('new-deck-form');
+
+  return {
+    clear() {
+      fileInput.value = null;
+    },
+
+    info() {
+      const data = new FormData(newDeckForm)
+      const config = {
+        headers: {
+          'content-type': 'multipart/form-data',
+        },
+      };
+
+      return { data, config };
+    }
+  };
 }
 
 function makeDiv(className) {
   const div = document.createElement('div');
   div.classList.add(className);
   return div;
+}
+
+function NewSpinner(event) {
+  const formId = event.target.id.replace('submit', 'form');
+  const form = document.getElementById(formId);
+
+  const shroud = makeDiv('shroud');
+  const spinner = makeDiv('spinner');
+  shroud.appendChild(spinner);
+
+  return {
+    show() {
+      form.appendChild(shroud);
+    },
+
+    remove() {
+      form.removeChild(shroud);
+    }
+  };
+}
+
+function pollStatus(taskStatus, spinner) {
+  let poll;
+  const handleStatus = () => {
+    axios.get(taskStatus)
+      .then(({ data }) => {
+        if (data.status !== 'processing') {
+          clearInterval(poll);
+          spinner.remove();
+          flash.show(data.status, data.message)
+          fileInput.clear();
+        }
+      });
+  };
+
+  poll = setInterval(handleStatus, 2000);
+}
+
+function submitForm() {
+  event.preventDefault();
+
+  const spinner = NewSpinner(event);
+  spinner.show();
+
+  const { data, config } = fileInput.info();
+
+  if (!data) return;
+
+  axios.post('/deck/new', data, config)
+    .then(({ data })=> {
+      pollStatus(data.taskStatus, spinner);
+    });
+}
+
+function wireUp(formId) {
+  getSubmitBtn(formId).addEventListener(
+    'click',
+    submitForm,
+  );
 }
--- src/db/ops.js
+++ src/db/ops.js
@@ -1,9 +1,17 @@
+import uuid from 'uuid/v1';
 import { processUpload } from 'Anki/processing';
 import { tryCatch } from 'Utils';
 import Card from 'Models/Card';
 
+const QUEUE = {};
+
 export default ({
   async addDeck(req, res) {
+    const taskId = uuid();
+    QUEUE[taskId] = 'processing';
+    res.set('Location', `/queue/${taskId}`);
+    res.status(202).json({ taskStatus: `/queue/${taskId}` });
+
     const filePath = req.file.path;
     const newCards = await tryCatch(processUpload(filePath));
 
@@ -25,5 +33,33 @@ export default ({
 
     await tryCatch(Card.bulkWrite(ops));
     console.log('Finished uploading/updating cards!');
+    QUEUE[taskId] = 'success';
   },
+
+  queueStatus(req, res) {
+    const taskId = req.params.taskId;
+    const status = QUEUE[taskId];
+    let message = '';
+
+    switch (status) {
+      case 'success':
+        delete QUEUE[taskId];
+        message = 'Deck uploaded successfully!';
+        break;
+      case 'error':
+        delete QUEUE[taskId];
+        message = 'ERROR: Something went wrong. Please try again.';
+        break;
+      case 'processing':
+        message = 'Check back later.';
+        break;
+      default:
+        status = 'warning';
+        message = 'Task not found.';
+        break;
+    }
+
+    res.json({ status, message });
+  },
+
 });
