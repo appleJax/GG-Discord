5fb84b9a1e3729c643f49249493968513a321f28
appleJax <appleJax@users.noreply.github.com>
2018-11-16 16:21:13 -0600

    adjust eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,9 +2,9 @@ module.exports = {
   "extends": "airbnb-base",
   "plugins": ["jest"],
   "rules": {
-    "semi": "always",
+    'import/no-unresolved': 0,
+    "import/prefer-default-export": 0,
     "no-param-reassign": 0,
     "no-use-before-define": 0,
-    'import/no-unresolved': 0,
   }
 };
--- src/db/models/Card.js
+++ src/db/models/Card.js
@@ -14,8 +14,8 @@ const schema = new Schema({
     [{
       _id: false,
       altText: String,
-      image: String
-    }], []
+      image: String,
+    }], [],
   ),
   questionText: String,
 });
