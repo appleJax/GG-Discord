b938855a8cfaab3534362f72a1211ac7f4a0c473
appleJax <appleJax@users.noreply.github.com>
2018-11-25 20:24:28 -0600

    tied correctAnswers have same rank

--- src/bot/updateLeaderboard.js
+++ src/bot/updateLeaderboard.js
@@ -16,10 +16,19 @@ export default async function updateLeaderboard(msg) {
 
   let stats = '**Top Scores:**\n';
   let nextUser = '';
-  let rank = 1;
+  let currentScore = -Infinity;
+  let skip = 1;
+  let rank = 0;
 
   for (const user of users) {
-    nextUser += `\n${rank++}. ${user.username}`;
+    if (user.correctAnswers > currentScore) {
+      rank += skip;
+      skip = 1;
+      currentScore = user.correctAnswers;
+    } else {
+      skip++;
+    }
+    nextUser += `\n${rank}. ${user.username}`;
     nextUser += await tryCatch(
       formatUserStats(user, roomCache),
     );
