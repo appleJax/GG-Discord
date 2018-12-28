d44856e7b6ba54caa3b5045ea90e552f8b1d6f37
appleJax <appleJax@users.noreply.github.com>
2018-12-28 16:26:18 -0600

    abstract createDeckStatsBox

---
+++ src/bot/updateLeaderboard/createDeckStatsBox.js
@@ -0,0 +1,60 @@
+import { Card } from 'Models';
+import { formatNumber, tryCatch } from 'Utils';
+import { percentage, RankCalculator } from 'Bot/utils';
+
+export default async function createDeckStatsBox(deck) {
+  const rankCalculator = RankCalculator();
+  const deckCards = await tryCatch(
+    Card.count({ deck: deck.name }).exec(),
+  );
+
+  let deckStats = '';
+  let deckUsers = deck.users.sort(
+    (a, b) => b.uniqueCardsCorrect.length - a.uniqueCardsCorrect.length,
+  );
+
+  deckStats += `\n${'```asciidoc'}\n= ${deck.name} =`;
+  deckStats += '\n\nTotal Cards Correct:';
+  deckStats += `\n(everyone: ${formatNumber(deck.correctAnswers)})`;
+
+  for (const user of deckUsers) {
+    const rank = rankCalculator.rank(user.correctAnswers);
+    deckStats += `\n${rank}. ${user.username}: ${formatNumber(user.correctAnswers)}`;
+  }
+
+  // TODO - abstract calculateUniqueCardsCorrect
+  deckUsers = deck.users.sort((a, b) => b.correctAnswers - a.correctAnswers);
+
+  if (deckUsers.length > 0) {
+    deckStats += `\n\nUnique Cards Correct (out of ${formatNumber(deckCards)}):`;
+  }
+
+  rankCalculator.reset();
+  for (const user of deckUsers) {
+    const rank = rankCalculator.rank(user.uniqueCardsCorrect.length);
+    deckStats += `\n${rank}. ${user.username}: ${formatNumber(user.uniqueCardsCorrect.length)} ${percentage(user.uniqueCardsCorrect.length, deckCards, user.deckLaps)} ${'🏆'.repeat(user.deckLaps)}`;
+  }
+
+  // TODO - abstract calculateSurvivalRecord
+  if (deck.survivalRecord > 0) {
+    deckStats += '\n\nSurvival Record:';
+    deckUsers = deck.users.concat({
+      username: 'everyone',
+      survivalRecord: deck.survivalRecord,
+    });
+  }
+
+  deckUsers = deckUsers
+    .filter(u => u.survivalRecord > 0)
+    .sort((a, b) => b.survivalRecord - a.survivalRecord);
+
+  rankCalculator.reset();
+  for (const user of deckUsers) {
+    const rank = rankCalculator.rank(user.survivalRecord);
+    deckStats += `\n${rank}. ${user.username}: ${formatNumber(user.survivalRecord)}`;
+  }
+
+  deckStats += '```';
+
+  return deckStats;
+}
--- src/bot/updateLeaderboard/index.js
+++ src/bot/updateLeaderboard/index.js
@@ -1,9 +1,9 @@
 /* eslint-disable prefer-template */
 
 import DECKS from 'Config/decks';
-import { Card, Deck, User } from 'Models';
-import { formatNumber, tryCatch } from 'Utils';
-import { percentage, RankCalculator } from 'Bot/utils';
+import { Deck, User } from 'Models';
+import { tryCatch } from 'Utils';
+import createDeckStatsBox from './createDeckStatsBox';
 import createOverallStatsBox from './createOverallStatsBox';
 import deleteOldLeaderboard from './deleteOldLeaderboard';
 import postNewLeaderboard from './postNewLeaderboard';
@@ -27,9 +27,6 @@ export default async function updateLeaderboard(channel) {
     createOverallStatsBox(everyone, users),
   );
 
-
-  const rankCalculator = RankCalculator();
-
   const decks = await tryCatch(
     Deck.find()
       .sort({ name: 'asc' })
@@ -37,63 +34,10 @@ export default async function updateLeaderboard(channel) {
       .exec(),
   );
 
-  let deckUsers;
-  let deckCards;
-
-  // TODO - abstract createDeckStatsBox(deck);
   for (const deck of decks) {
-    // TODO - abstract calculateCorrectAnswers
-    deckUsers = deck.users.sort(
-      (a, b) => b.uniqueCardsCorrect.length - a.uniqueCardsCorrect.length,
+    stats += await tryCatch(
+      createDeckStatsBox(deck),
     );
-
-    deckCards = await tryCatch(
-      Card.count({ deck: deck.name }).exec(),
-    );
-
-    stats += `\n${'```asciidoc'}\n= ${deck.name} =`;
-    stats += '\n\nTotal Cards Correct:';
-    stats += `\n(everyone: ${formatNumber(deck.correctAnswers)})`;
-
-    rankCalculator.reset();
-    for (const user of deckUsers) {
-      const rank = rankCalculator.rank(user.correctAnswers);
-      stats += `\n${rank}. ${user.username}: ${formatNumber(user.correctAnswers)}`;
-    }
-
-    // TODO - abstract calculateUniqueCardsCorrect
-    deckUsers = deck.users.sort((a, b) => b.correctAnswers - a.correctAnswers);
-
-    if (deckUsers.length > 0) {
-      stats += `\n\nUnique Cards Correct (out of ${formatNumber(deckCards)}):`;
-    }
-
-    rankCalculator.reset();
-    for (const user of deckUsers) {
-      const rank = rankCalculator.rank(user.uniqueCardsCorrect.length);
-      stats += `\n${rank}. ${user.username}: ${formatNumber(user.uniqueCardsCorrect.length)} ${percentage(user.uniqueCardsCorrect.length, deckCards, user.deckLaps)} ${'🏆'.repeat(user.deckLaps)}`;
-    }
-
-    // TODO - abstract calculateSurvivalRecord
-    if (deck.survivalRecord > 0) {
-      stats += '\n\nSurvival Record:';
-      deckUsers = deck.users.concat({
-        username: 'everyone',
-        survivalRecord: deck.survivalRecord,
-      });
-    }
-
-    deckUsers = deckUsers
-      .filter(u => u.survivalRecord > 0)
-      .sort((a, b) => b.survivalRecord - a.survivalRecord);
-
-    rankCalculator.reset();
-    for (const user of deckUsers) {
-      const rank = rankCalculator.rank(user.survivalRecord);
-      stats += `\n${rank}. ${user.username}: ${formatNumber(user.survivalRecord)}`;
-    }
-
-    stats += '```';
   }
 
   const leaderboard = channel.client.channels.get(DECKS.leaderboard);
