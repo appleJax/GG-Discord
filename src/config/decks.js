3403c61eb30c55c4d70420bae163fefb74aabbc2
appleJax <appleJax@users.noreply.github.com>
2019-01-01 22:17:32 -0600

    add English quiz room

--- src/config/decks.js
+++ src/config/decks.js
@@ -1,6 +1,7 @@
 /* eslint-disable */
 let DECKS = {
   '504554082984525854': 'Gamegogakuen JP',
+  '': 'Gamegogakuen EN',
   '505262797572276254': 'DBJG',
   '448017333907095562': 'DIJG',
   '507815046886457344': 'iKnow Core 2000',
@@ -25,6 +26,7 @@ let DECKS = {
 if (process.env.NODE_ENV !== 'production') {
   DECKS = {
     '512832378427932692': 'Gamegogakuen JP',
+    '529871415663656981': 'Gamegogakuen EN',
     '512832466575294484': 'DBJG',
     '512832494173945857': 'DIJG',
     '512832558300397589': 'iKnow Core 2000',
