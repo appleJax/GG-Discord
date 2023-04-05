bb9214c26aeead839355d9203104f709833c6a67
appleJax <appleJax@users.noreply.github.com>
2023-04-04 17:15:24 -0700

    fix deleteOldLeaderboard helper

--- src/bot/updateLeaderboard/deleteOldLeaderboard.js
+++ src/bot/updateLeaderboard/deleteOldLeaderboard.js
@@ -4,6 +4,6 @@ export default async function deleteOldLeaderboard(leaderboard) {
   const oldMessages = await tryCatch(leaderboard.messages.fetch());
 
   if (oldMessages.size) {
-    await Promise.all(oldMessages.deleteAll());
+    await Promise.all(oldMessages.each((msg) => msg.delete()));
   }
 }
